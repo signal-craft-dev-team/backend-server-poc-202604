ab1ea96158848d7c30e12865dacceb8050b3fa84
James0103 <jameshhjung@gmail.com>
2026-03-31 16:45:02 +0900

    feat: mqtt 코드 수정

--- app/mqtt/client.py
+++ app/mqtt/client.py
@@ -1,13 +1,21 @@
 import os
-import paho.mqtt.client as mqtt
 
-BROKER_HOST = os.getenv("MQTT_BROKER_HOST", "localhost")
-BROKER_PORT = int(os.getenv("MQTT_BROKER_PORT", 1883))
-CLIENT_ID = os.getenv("MQTT_CLIENT_ID", "signal-craft-backend")
+import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BROKER_HOST = os.getenv("MQTT_HOST", "localhost")
+BROKER_PORT = 1883
+MQTT_USER = os.getenv("MQTT_USER")
+MQTT_PWD = os.getenv("MQTT_PWD")
+CLIENT_ID = "signal-craft-backend"
 
 
 def create_client() -> mqtt.Client:
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id=CLIENT_ID)
+    if MQTT_USER and MQTT_PWD:
+        client.username_pw_set(MQTT_USER, MQTT_PWD)
     return client
 
 
